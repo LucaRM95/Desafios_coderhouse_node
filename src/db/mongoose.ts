67d2c1142a3cd1a1e0908b063fd00d5fb52e1125
Luca Rojas Massey <lucasrojas95@gmail.com>
2023-10-29 10:53:04 -0300

    Refactorización de conexión con dv

---
+++ src/db/mongoose.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const USER = process.env.USER;
+const PASSWORD = process.env.PASSWORD;
+const database = "ecommerce";
+const URI=`mongodb+srv://${USER}:${PASSWORD}@cluster0.khrlasb.mongodb.net/${database}`;
+
+export const init = async () => {
+  try {
+    await mongoose.connect(URI);
+    console.log('Database conected 🚀');
+  } catch (error) {
+    console.log('Ah ocurrido un error al intentar conectarnos a la DB', error);
+  }
+}
